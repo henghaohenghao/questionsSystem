4c3a896d7f14847058adbf0cc7305364853a5fa2
tom_glb <goooMorning@atguigu.com>
2022-12-07 20:59:18 +0800

    Initial commit

---
+++ neo_db/creat_graph.py
@@ -0,0 +1,24 @@
+from py2neo import Graph, Node, Relationship,NodeMatch
+from config import graph
+with open("../raw_data/relation.txt",encoding='utf-8') as f:
+    for line in f.readlines():
+        rela_array=line.strip("\n").split(",")
+        print(rela_array)
+        graph.run("MERGE(p: Person{cate:'%s',Name: '%s'})"%(rela_array[3],rela_array[0]))
+        graph.run("MERGE(p: Person{cate:'%s',Name: '%s'})" % (rela_array[4], rela_array[1]))
+        graph.run(
+            "MATCH(e: Person), (cc: Person) \
+            WHERE e.Name='%s' AND cc.Name='%s'\
+            CREATE(e)-[r:%s{relation: '%s'}]->(cc)\
+            RETURN r" % (rela_array[0], rela_array[1], rela_array[2],rela_array[2])
+
+        )
+
+# from py2neo import Node, Relationship, Graph
+#
+# a = Node('Person', name='Alice')
+# b = Node('Person', name='Bob')
+# r = Relationship(a, 'KNOWS', b)
+# s = a | b | r
+# graph = Graph(password='123456')
+# graph.create(s)
