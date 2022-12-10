3735c800215d89fc63d623aaba1a9708dde781e2
otahwts <otahwts@outlook.com>
2022-12-10 17:14:04 +0800

    master

--- spider/get_character_array.py
+++ spider/get_character_array.py
@@ -1,11 +1,15 @@
+#获取人物(角色)
+
 import codecs
+
 def get_character():
-    f = codecs.open('./raw_data/relation.txt','r','utf-8')
+    f = codecs.open('../raw_data/relation.txt','r','utf-8')
     data = []
     for line in f.readlines():
+        #strip用于移除字符串头尾指定的字符或字符序列,默认为空格
         array = line.strip("\n").split(",")
         arr = [array[0],array[1]]
+        #extend将传入的对象参数中的元素序列追加到列表末尾
         data.extend(arr)
-    
-    return data
 
+    return data
--- spider/get_hlm_character.py
+++ spider/get_hlm_character.py
@@ -18,9 +18,11 @@ headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWe
 
 def get_json(character_arr):
 	data={}
+    #用于获取每个人物的百科网页详情
 	for i in set(character_arr):
 		print(i)
 		url=r'https://baike.baidu.com/item/'+i
+        #urllib.parse中的quote方法能够将汉字转换成unicode编码的格式,适用于单个参数
 		url = quote(url, safe = string.printable)
 		req = request.Request(url, headers=headers)
 		response = request.urlopen(req, timeout=20)
@@ -46,5 +48,10 @@ def get_json(character_arr):
 	f.write(json.dumps(data,  ensure_ascii=False))
 if __name__ == "__main__":
 	character_arr=get_character()
+    #os.chdir()方法用于改变当前工作目录到指定的路径
+    #os.path.join()函数用于路径拼接文件路径,可以传入多个路径
+        #从后往前看,会从第一个以"/"开头的参数开始拼接,之前的参数全部丢弃
+        #以上一种情况为先.在上一种情况确保情况下,若出现"./"开头的参数,会从"./"开头的参数的前面参数全部保留
+    #os.getcwd()的功能是获得当前文件的路径
 	os.chdir(os.path.join(os.getcwd(), './spider/images'))
 	get_json(character_arr)
